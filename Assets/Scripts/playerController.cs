96b485c82dee0d733c6fad29e1a9556ad38fb64f
javiermartinez27 <50684376+javiermartinez27@users.noreply.github.com>
2019-09-09 18:26:42 -0300

    Primer commit

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+	public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+    	Vector3 movement = new Vector3(Input.GetAxis("MoveHorizontal"), Input.GetAxis("MoveVertical"), 0.0f);
+    	//animaciones
+    	transform.position = transform.position + movement * speed * Time.deltaTime;
+    }
+}
